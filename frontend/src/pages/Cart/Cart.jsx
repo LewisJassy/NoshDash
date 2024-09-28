808fd45ddd4108a2157831c0516637d006d3af89
LewisJassy <jassylewis70@gmail.com>
2024-09-28 10:55:09 +0300

    Refactor Cart component to optimize rendering of cart items

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import { useContext } from 'react'
 import './Cart.css'
 import { StoreContext } from '../../components/context/StoreContext'
 import { useNavigate } from 'react-router-dom';
@@ -21,21 +21,21 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-        {food_list.map((item,index)=>{
-          if(cartItems[item._id]>0){
-            return <div>
-              <div className="cart-items-title cart-items-item">
-              <img src={url+'/images/'+item.image} alt="" />
-              <p>{item.name}</p>
-              <p>${item.price}</p>
-              <p>{cartItems[item._id]}</p>
-              <p>${item.price*cartItems[item._id]}</p>
-              <p onClick={()=>removeFromCart(item._id)} className='cross'>x</p>
-            </div>
-            <hr />
-            </div>
-
-            
+        {food_list.map((item) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div key={item._id}>
+                <div className="cart-items-title cart-items-item">
+                  <img src={url + '/images/' + item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className='cross'>x</p>
+                </div>
+                <hr />
+              </div>
+            );
           }
         })}
       </div>
