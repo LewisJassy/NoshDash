7b95c059d2d7628a8a656cb1dcd4b26f1f367b48
LewisJassy <jassylewis70@gmail.com>
2025-01-14 17:28:38 +0300

    Fix API URL in StoreContext by removing trailing slash

--- frontend/src/components/context/StoreContext.jsx
+++ frontend/src/components/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "https://noshdash.onrender.com/";
+    const url = "https://noshdash.onrender.com";
     const [token,setToken] = useState("");
 
     const [food_list, setFoodList] = useState([]);
